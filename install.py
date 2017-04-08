0c295f1dcf862ceda9a4a29e706aba2fae99001f
Dmitry S. Fedorov <fedusia@yandex-team.ru>
2017-04-08 15:35:54 +0300

    install.py

--- install.py
+++ install.py
@@ -5,19 +5,47 @@ import os
 import subprocess
 
 
+REPO = 'https://github.com/fedusia/common-dot-files'
+WORKING_DIR = 'test'
+ENV_FILES = ['bashrc', 'vimrc', 'gitconfig']
+
+
+def get_osname():
+    return platform.system()
+
+
+def get_cwd():
+    return os.getcwd()
+
+
+def create_env_root_dir():
+    home_dir = os.path.expanduser("~")
+    env_dir = home_dir + '/' + WORKING_DIR
+    os.makedirs(env_dir, exist_ok=True)
+    return env_dir
+
+
 def main():
-    distName = platform.dist()[0]
-    if distName == 'Ubuntu':
-        retval = subprocess.call(['which', 'git'])
-        print(retval)
-
-        if retval:
-            subprocess.call(['apt-get', 'install', 'git'])
-
-        if not os.path.exists('~/work1'):
-            os.makedirs = ('~/work1')
-        subprocess.call(['git', 'clone', 'http://github.com/fedusia/common-dot-files', '~/work1'])
-
+    env_dir = create_env_root_dir()
+    if get_cwd() is not env_dir:
+        os.chdir(env_dir)
+        os.makedirs('src/github.com', exist_ok=True)
+        try:
+            subprocess.check_call(['git', 'clone', REPO, './src/github.com/fedusia/common-dot-files'])
+        except subprocess.CalledProcessError as err:
+            print('hello')
+            print(err)
+        for file in ENV_FILES:
+            if get_osname() == 'Darwin' and file == 'bashrc':
+                try:
+                    os.symlink(env_dir + '/src/github.com/fedusia/common-dot-files/' + file, '../.bash_profile')
+                except os.error as err:
+                    print('Link to {0} already exists: '.format(file), err.strerror)
+                    continue
+            try:
+                os.symlink(env_dir + '/src/github.com/fedusia/common-dot-files/' + file, '../.' + file)
+            except os.error as err:
+                print('Link to {0} already exists: '.format(file), err.strerror)
 
 if __name__ == '__main__':
     main()
